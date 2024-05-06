bddf21e6f2bcba44362b40658d7069bcefd2a7cc
Sudhakar Reddy Medagam <msudhakarreddy09@gmail.com>
2024-05-06 21:11:22 +0530

    Initial commit

---
+++ resume/skills.js
@@ -0,0 +1,43 @@
+window.mySkills = {
+    "skills": [
+        { "name": "HTML/HTML5", "per": "95%"},
+        { "name": "Javascript", "per": "85%"},
+        { "name": "jQuery", "per": "85%"},
+        { "name": "CSS/CSS3", "per": "80%"},
+        { "name": "Angular js", "per": "85%"},
+        { "name": "Angular", "per": "75%"},
+        { "name": "React", "per": "75%"},
+        { "name": "React Native", "per": "65%"},
+        { "name": "SAPUI5", "per": "60%"},
+        { "name": "NodeJs", "per": "70%"},
+        { "name": "Bacbkone", "per": "60%"},
+        { "name": "NetSuite", "per": "80%"},
+        { "name": "Jasmine", "per": "90%"},
+        { "name": "Mocha", "per": "80%"},
+        { "name": "Jest", "per": "80%"},
+        { "name": "Git", "per": "90%"},
+        { "name": "Perforce", "per": "90%"},
+        { "name": "Python", "per": "40%"},
+        { "name": "Java", "per": "45%"},
+        { "name": "YUI3", "per": "65%"},
+        { "name": "OOJS/ES6", "per": "80%"},
+        { "name": "Responsive Web design", "per": "75%"},
+        { "name": "SASS/LESS", "per": "75%"},
+        { "name": "Splunk Logger", "per": "60%"},
+        { "name": "Machine Learning", "per": "30%"},
+        { "name": "GraphQL", "per": "60%"},
+
+    ],
+    "companies_template_obj":[
+        {"c_name": "Oracle", "DOJ": "May 2018 - Till Date", "exp": "Present", "logo":"https://media-exp1.licdn.com/dms/image/C4E0BAQGmS4ZS9i2T6w/company-logo_100_100/0?e=1608163200&v=beta&t=XEDgyJOSjz34c-nIvjjv6bT2xJ8ZkSO22aTbGgL4-tw", "current_role": "Development Manager", "past_roles": "", "location": "Hyderabad, India", "other_loacations": ""},
+        {"c_name": "Wells Fargo", "DOJ": "Jan 2015 – Apr 2018", "exp": "3yr 4m", "logo":"https://media-exp1.licdn.com/dms/image/C560BAQHeDOdNfmP_dg/company-logo_200_200/0?e=1608163200&v=beta&t=TkIiJD1ElN21rpSxn37MWQbbVW_IyHa83KrEtLSvKHE", "current_role": "Technology Specialist", "past_roles": "", "location": "Hyderabad, India", "other_loacations": ""},
+        {"c_name": "Prokarma Softech", "DOJ": "Apr 2012 – Jan 2015", "exp": "2yr 10m", "logo":"https://media-exp1.licdn.com/dms/image/C4E0BAQGXSYbjpauK7Q/company-logo_200_200/0?e=1608163200&v=beta&t=VFDSGDeZIn4uWF0mYlEa3qzmyY8ZDzxWmiFWCawBJXE", "current_role": "Sr. Software Engineer", "past_roles": "", "location": "Hyderabad, India", "other_loacations": ""},
+        {"c_name": "FireFlys Technologies", "DOJ": "June 2010 - March 2012", "exp": "1yr 10m", "logo":"", "current_role": "Software Engineer", "past_roles": "", "location": "Hyderabad, India", "other_loacations": ""}
+    ],
+    "education": [
+        {"course": "Master of Computer Apllications", "year": "2010", "institute": "Hindustan University", "per": "7.8", "location": "Chennai, Tamilanadu"},
+        {"course": "B.S.C computers", "year": "2007", "institute": "Sriji College", "per": "7.8", "location": "Andhrapradesh"},
+        {"course": "Intermediate", "year": "2004", "institute": "MSR Jr College", "per": "8.4", "location": "Andhrapradesh"},
+        {"course": "SSC", "year": "2002", "institute": "Saraswathi High school", "per": "7.8", "location": "Andhrapradesh"}
+    ]
+}
